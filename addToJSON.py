da5c98542e527ccb6c0b0bd30628b6caa7bfab57
George <gwbarnesspam@gmail.com>
2025-02-11 16:17:44 +0000

    edit json complete

--- addToJSON.py
+++ addToJSON.py
@@ -1,12 +1,10 @@
 import json
 
-def appendJSON(fileName, data):
+def addToJSON(fileName, data):
     with open(fileName, 'r') as file: #load current file
         currentData = json.load(file)
     if isinstance(currentData, dict):  # Ensure the data structure is a dict
-        print(currentData)
         currentData.update(data)
-        print(currentData)
     else:
         print("JSON file not dict")
         return None
